f8a65136635ffa6acd79dd119f45d864c1434568
Ananya <65886348+Ananyaiitbhilai@users.noreply.github.com>
2021-04-27 00:34:23 +0530

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'mywebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ '/Users/aruny/mywebsite/templates',],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
