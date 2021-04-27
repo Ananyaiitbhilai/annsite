bc0f3da8436bf8025c8a4a11717041db081b756c
Ananya <65886348+Ananyaiitbhilai@users.noreply.github.com>
2021-04-27 11:01:45 +0530

    Update models.py

--- mywebsite/models.py
+++ mywebsite/models.py
@@ -1,7 +1,7 @@
 
 from django.db import models
 
-class mywebsite_signupform(models.Model):
+class signupform(models.Model):
     user_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=50)
     user_email = models.EmailField()
